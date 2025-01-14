fe9328f5294189b32c40dbbf62f962c5a8baace4
makh <kmohammadali66@yahoo.com>
2025-01-14 20:21:37 +0330

    added .env & .dockerignore & modified df.js & server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import { ENV_VARS } from "./envVars.js";
+//import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+		//const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
