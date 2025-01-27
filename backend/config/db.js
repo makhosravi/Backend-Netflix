1e44652748c669894e67657c4ef29a67b2b64a0f
makh <kmohammadali66@yahoo.com>
2025-01-28 00:03:35 +0330

    refactor docker-compose & db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,13 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 	try {
 		//const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(() => {
+		  console.log('MongoDB connected successfully');
+		})
+		.catch((error) => {
+		  console.error('Error connecting to MongoDB:', error);
+		});
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
