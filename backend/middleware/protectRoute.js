13a7605d91b482809960cc015461c89c1a1df667
makh <kmohammadali66@yahoo.com>
2025-02-14 15:55:47 +0330

    bug: added logger to find problem in protectRoute

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -7,7 +7,7 @@ import { User } from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.headers["authorization"];
-		console.log("authorization in header: ", req.headers["authorization"]);
+		console.log("authorization in header: ", token);
 		
 		if (!token) {
 			return res.status(401).json({ success: false, message: "Unauthorized - No Token Provided" });
@@ -15,7 +15,13 @@ export const protectRoute = async (req, res, next) => {
 
 		console.log("protectRoute ==> passed token test!");
 
-		const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+		// Ensure proper format
+		const tokenParts = token.split(" ");
+		if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+    		return res.status(401).json({ success: false, message: "Unauthorized - Invalid Token Format" });
+		}
+
+		const decoded = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
 
 		if (!decoded) {
 			return res.status(401).json({ success: false, message: "Unauthorized - Invalid Token" });
@@ -23,7 +29,13 @@ export const protectRoute = async (req, res, next) => {
 
 		console.log("protectRoute ==> passed decode test!");
 
-		const user = await User.findById(decoded.userId).select("-password");
+		let user;
+        try {
+            user = await User.findById(decoded.userId).select("-password");
+        } catch (err) {
+            console.log("protectRoute -> Database error:", err);
+            return res.status(500).json({ success: false, message: "Database Error" });
+        }
 
 		if (!user) {
 			return res.status(404).json({ success: false, message: "User not found" });
@@ -35,7 +47,12 @@ export const protectRoute = async (req, res, next) => {
 
 		console.log("protectRoute ==> passed assinging user!");
 
-		next();
+		try {
+            next();
+        } catch (err) {
+            console.log("Error after calling next():", err);
+            res.status(500).json({ success: false, message: "Middleware Error" });
+        }
 
 		console.log("protectRoute ==> passed running next func!");
 	} catch (error) {
