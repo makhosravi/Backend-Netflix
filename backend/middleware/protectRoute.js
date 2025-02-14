08f55485a60e9927c291886c20b97e4576bb5cc7
makh <kmohammadali66@yahoo.com>
2025-02-14 15:32:09 +0330

    bug: added logger to find problem in protectRoute

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -7,6 +7,7 @@ import { User } from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.headers["authorization"];
+		console.log("authorization in header: ", req.headers["authorization"]);
 		
 		if (!token) {
 			return res.status(401).json({ success: false, message: "Unauthorized - No Token Provided" });
