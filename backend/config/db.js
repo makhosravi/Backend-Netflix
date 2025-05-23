a71225a68a88f1180130a03c3ddb50e417517303
makh <kmohammadali66@yahoo.com>
2025-05-23 14:57:05 +0330

    bugfix: solved the problem in db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,10 +11,11 @@ export const connectDB = async () => {
 	try {
 		//const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
 		console.log(process.env.MONGO_URI);
-		const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+		const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 		console.log("MongoDB connected: " + conn.connection.host);
-	} catch (error) {
+	} 
+	catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
-		process.exit(1);
+		setTimeout(connectDB, 5000);
 	}
 };
