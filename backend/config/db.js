02e2076ddb6e1dc1013745bea8189b6e6ff1e982
makh <kmohammadali66@yahoo.com>
2025-05-23 15:27:38 +0330

    refactor: changed db connection retry mechanism

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 //import { ENV_VARS } from "./envVars.js";
 
+let retryCount = 0;
+const maxRetries = 5;
+
 export const connectDB = async () => {
 	if (!process.env.MONGO_URI) {
 		console.error("MONGO_URI is not defined in the environment variables.");
@@ -13,11 +16,16 @@ export const connectDB = async () => {
 		console.log(process.env.MONGO_URI);
 		const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 		console.log("MongoDB connected: " + conn.connection.host);
-		process.exit(1); 
 	} 
 	catch (error) {
+		retryCount++;
 		console.error("Error connecting to MONGODB: " + error.message);
-		setTimeout(connectDB, 5000);
+		if (retryCount < maxRetries) {
+      		console.log(`Retrying connection (${retryCount}/${maxRetries}) in 5 seconds...`);
+      		setTimeout(connectDB, 5000);
+    	} else {
+      		console.error("Max retries reached. Exiting...");
+      		process.exit(1);
+    	}
 	}
-	process.exit(1); 
 };
